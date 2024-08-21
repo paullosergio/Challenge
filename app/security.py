1a8f6109f71759f0304560dd2625eb325947f9eb
paullosergio <paulomg1996@gmail.com>
2024-08-20 22:11:03 -0300

    Add security

---
+++ app/security.py
@@ -0,0 +1,83 @@
+from datetime import datetime, timedelta
+from http import HTTPStatus
+
+import requests
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jwt import decode, encode
+from jwt.exceptions import ExpiredSignatureError, PyJWTError
+from pwdlib import PasswordHash
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from zoneinfo import ZoneInfo
+
+from app.database import get_session
+from app.models import User
+from app.settings import Settings
+
+pwd_context = PasswordHash.recommended()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='auth/token')
+settings = Settings()
+
+
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.now(tz=ZoneInfo('UTC')) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode['exp'] = expire
+
+    return encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+
+def get_current_user(
+    session: Session = Depends(get_session),
+    token: str = Depends(oauth2_scheme),
+):
+    credentials_exception = HTTPException(
+        status_code=HTTPStatus.UNAUTHORIZED,
+        detail='Could not validate credentials',
+        headers={'WWW-Authenticate': 'Bearer'},
+    )
+
+    try:
+        payload = decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms=[settings.ALGORITHM],
+        )
+        username: str = payload.get('sub')
+        if not username:
+            raise credentials_exception
+
+    except ExpiredSignatureError:
+        raise credentials_exception
+
+    except PyJWTError:
+        raise credentials_exception
+
+    if user := session.scalar(select(User).where(User.email == username)):
+        return user
+    else:
+        raise credentials_exception
+
+
+def validate_cpf(cpf: str) -> bool:
+    url = 'https://api.invertexto.com/v1/validator'
+    params = {'token': settings.API_TOKEN, 'value': cpf, 'type': 'cpf'}
+
+    response = requests.get(url, params=params)
+
+    if response.status_code != HTTPStatus.OK:
+        response.raise_for_status()  # pragma: no cover
+
+    result = response.json()
+    return result.get('valid', False)

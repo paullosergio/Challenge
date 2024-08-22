9024b184b463a0177a4882ed9553600c734786cb
paullosergio <paulomg1996@gmail.com>
2024-08-22 14:28:02 -0300

    Update logging

--- app/logging_config.py
+++ app/logging_config.py
@@ -2,7 +2,7 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s',
+    format='%(levelname)s:     %(asctime)s - %(message)s',
     handlers=[logging.StreamHandler()],
 )
 
